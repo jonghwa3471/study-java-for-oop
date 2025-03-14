1a97c4c8468677f21818085491f5c2dd8810f123
Jonghwa <whdghk3471@gmail.com>
2025-03-14 11:15:37 +0900

    modified

--- src/ex1/App.java
+++ src/ex1/App.java
@@ -5,7 +5,6 @@ public class App {
         int u_price = -300;
         int count = 9;
 
-        // 정상적인 printf 사용
         System.out.printf("%d원짜리 %d개를 사면 %d입니다\n", 300, 9, 300 * 9);
         System.out.printf("%10d원짜리 %+10d개를 사면 %10d입니다\n", u_price, count, u_price * count);
         System.out.printf("10진수 15를 16진수로 출력하면 : %x, 8진수로 출력하면 : %o\n", 15, 15);
