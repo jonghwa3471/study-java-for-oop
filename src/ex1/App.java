623c971c7161887cd7d53a4018139c7d4370ece3
Jonghwa <whdghk3471@gmail.com>
2025-05-23 13:44:02 +0900

    week 12

--- src/ex1/App.java
+++ src/ex1/App.java
@@ -1,17 +1,26 @@
 package ex1;
-import java.io.*;
-import java.util.Scanner;
+
+import java.util.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner stdin = new Scanner(System.in);
-        System.out.println("읽어 들일 파일명을 입력하세요 : ");
-        String s = stdin.next();
-        FileReader fr = new FileReader(s);
-        int i;
-        while((i = fr.read()) != -1) {
-            System.out.println((char)i);
+    public static void main(String[] args) {
+        LinkedHashMap<String, Integer> lm = new LinkedHashMap<String, Integer>();
+        lm.put("국어", 98);
+        lm.put("영어", 100);
+        lm.put("수학", 86);
+        lm.put("사회", 99);
+        lm.put("과학", 89);
+        System.out.println("당신의 성적 : " + lm + total_avg(lm));
+    }
+
+    public static String total_avg(Map m) {
+        int total = 0;
+        int count = m.size();
+        Collection<Integer> c = m.values();
+        Iterator<Integer> iterator = c.iterator();
+        while (iterator.hasNext()) {
+            total += iterator.next();
         }
-        fr.close();
+        return "총점 =" + total + " 평균 =" + (double)total/count;
     }
 }
